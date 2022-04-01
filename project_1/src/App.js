6c47139f78a777980b533006c7ef33ef05ba45d5
Roman Laputyn <laputyn.roman@gmail.com>
2022-04-01 16:46:02 +0200

    changed css

--- project_1/src/App.js
+++ project_1/src/App.js
@@ -1,6 +1,6 @@
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Main from './Components/Main';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Main from './Components/Main/Main';
 
 function App() {
   return (
